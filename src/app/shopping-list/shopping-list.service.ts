f4575c5337c2c10b2125c22b716c7f7f194074f3
bramah22 <bramah22@gmail.com>
2018-09-25 19:12:41 +0100

    Ajout des Observables dans le Shopping-list pour l'ajout d'ingredients

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,24 +1,26 @@
 import {Ingredient} from '../shared/ingredient.model';
 import {EventEmitter} from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
+
   ingredients: Ingredient[] = [
     new Ingredient('Pomme' , 5),
     new Ingredient('Tomates' , 10)
   ] ;
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   getIngredients() {
     return this.ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients);
+    this.ingredientsChanged.next(this.ingredients);
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
